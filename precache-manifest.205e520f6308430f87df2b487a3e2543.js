52d0e47f74fb0bc0621f711be6896a01670a3102
Hahmjimin <80664856+Hahmjimin@users.noreply.github.com>
2022-06-29 21:37:18 +0900

    Updates

---
+++ precache-manifest.205e520f6308430f87df2b487a3e2543.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3581f4060cc5900dcd5",
+    "url": "/react_project/static/css/main.81c08d61.chunk.css"
+  },
+  {
+    "revision": "a3581f4060cc5900dcd5",
+    "url": "/react_project/static/js/main.a3581f40.chunk.js"
+  },
+  {
+    "revision": "20f555196c3a35a2c299",
+    "url": "/react_project/static/js/1.20f55519.chunk.js"
+  },
+  {
+    "revision": "8385a62ee4aa16b69f15",
+    "url": "/react_project/static/js/runtime~main.8385a62e.js"
+  },
+  {
+    "revision": "3c5238cd6265cbd2f8aae7b69084ea0f",
+    "url": "/react_project/index.html"
+  }
+];
