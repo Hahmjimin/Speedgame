0a30668ee149c8d3c38e09a1e4c833b813d48095
Hahmjimin <80664856+Hahmjimin@users.noreply.github.com>
2022-06-30 16:43:01 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react_project/precache-manifest.2443473673c3b7555c0fb7728b1a9c05.js"
+  "/react_project/index.html/precache-manifest.f8815adcd3ca953500bc96721a645eae.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react_project/index.html", {
+workbox.routing.registerNavigationRoute("/react_project/index.html/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
