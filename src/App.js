d7379df3fb481f7ef0f5cf08a880c996191c832e
Hahmjimin <80664856+Hahmjimin@users.noreply.github.com>
2022-06-30 16:58:15 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import OneToFifty from "./components/OneToFifty";
 
 
 export default function App() {
   return (
-    // <OneToFifty></OneToFifty>
-    <Router>
-      <Routes>
-        {/* <Route path="Main" element={<Main />} /> */}
-          <Route path="OneToFifty" element={<OneToFifty />} />
-      </Routes>
-    </Router>
+    <OneToFifty/>
   );
 }
